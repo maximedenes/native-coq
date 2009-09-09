a71186e5b8b5ab4625b652c86e7588bbd7fe65c3
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-09-09 14:30:34 +0200

    Unplug the VM in vconv.ml and call into the NbE stuff of nativecode.ml4 instead.

--- kernel/vconv.ml
+++ kernel/vconv.ml
@@ -238,16 +238,13 @@ and conv_eq_vect vt1 vt2 cu =
   else raise NotConvertible
 
 let vconv pb env t1 t2 =
-  let cu =
-    try conv_eq pb t1 t2 Constraint.empty
+    try print_endline "blah1"; conv_eq pb t1 t2 Constraint.empty
     with NotConvertible ->
-      infos := create_clos_infos betaiotazeta env;
-      let v1 = val_of_constr env t1 in
-      let v2 = val_of_constr env t2 in
-      let cu = conv_val pb (nb_rel env) v1 v2 Constraint.empty in
-      cu
-  in cu
-
+      print_endline "notconvert";
+      let values = Nativecode.compile env t1 t2 in
+      let cu = Nativecode.compare values Constraint.empty in
+        print_endline "blah"; cu
+    
 let _ = Reduction.set_vm_conv vconv
 
 let use_vm = ref false
@@ -258,5 +255,3 @@ let set_use_vm b =
   else Reduction.set_default_conv Reduction.conv_cmp
 
 let use_vm _ = !use_vm
-
-
