710cb20b7d29361e6bf79b732b65822d1151d1e8
Maxime Dénès <maxime.denes@lix.polytechnique.fr>
2010-07-12 18:31:49 +0200

    Refactored code. Removed Nbeconv dependency in Nativenorm. Renamed Nbeconv to Nativeconv.

--- kernel/nativecode.mli
+++ kernel/nativecode.mli
@@ -12,9 +12,12 @@ val opaque_const : constant -> MLast.str_item list
 
 val assums : env -> constr -> string list
 
-val add_dummy_loc : MLast.str_item list -> (MLast.str_item * Ploc.t) list
-
 val translate_ind : env ->
            Names.inductive ->
            Declarations.mutual_inductive_body *
-           Declarations.one_inductive_body -> (MLast.str_item * Ploc.t) list
+           Declarations.one_inductive_body -> MLast.str_item list
+
+val dump_env :
+  constr list -> env -> MLast.str_item list * 
+      (Names.id_key * NbeAnnotTbl.t) Util.Stringmap.t
+
