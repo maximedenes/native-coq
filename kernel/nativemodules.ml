65757063c65efece1fc4c48cfe9617aaaf1805ce
Maxime Dénès <Maxime.Denes@inria.fr>
2010-12-09 13:42:03 +0100

    Compilation of functors and signatures.

--- kernel/nativemodules.ml
+++ kernel/nativemodules.ml
@@ -8,56 +8,58 @@ open Modops
 open Nativecode
 open Nativelib
 
+type mod_sig_field =
+  | MSFconst of label
+
+and mod_sig_expr =
+  MSEsig of mod_sig_field list
+
 type mod_field =
   | MFglobal of global
   | MFmodule of label * mod_expr
-  | MFmodtype
+  | MFmodtype of label * mod_sig_expr
 
 and mod_expr =
   | MEident
   | MEstruct of mod_field list
-  | MEfunctor
+  | MEfunctor of string * mod_sig_expr * mod_expr
 
 let rec translate_mod_type mp env typ_expr =
-  assert false
-(*  match typ_expr with
-  | SEBident mp ->
-      <:module_type< $uid:string_of_mp mp$ >>
+  match typ_expr with
+  | SEBident mp -> assert false
+(*      <:module_type< $uid:string_of_mp mp$ >>*)
   | SEBstruct expr_list ->
-      let ast = List.map (translate_fields_type mp env) expr_list in
-      <:module_type< sig $list:ast$ end >>
-  | SEBfunctor (mbid, mtb, mtb') ->
+      MSEsig (List.map (translate_fields_type mp env) expr_list)
+  | SEBfunctor (mbid, mtb, mtb') -> assert false
+      (*
       let mp' = MPbound mbid in
       let env' = add_module (module_body_of_type mp' mtb) env in
       let (_,mbid,_) = repr_mbid mbid in
       let ast = translate_mod_type mp env' mtb' in
       let typ_ast = translate_mod_type mp' env mtb.typ_expr in
-      <:module_type< functor ($uid:mbid$ : $typ_ast$) -> $ast$ >>
-  | _ -> assert false*)
+      <:module_type< functor ($uid:mbid$ : $typ_ast$) -> $ast$ >> *)
+  | _ -> assert false
 
 and translate_fields_type mp env (l,e) =
-  assert false
-(*  match e with
-  | SFBconst cb ->
-      let _,lid = const_lid mp (make_con mp empty_dirpath l) in
-      <:sig_item< value $lid:lid$ : Nativevalues.t >>
-  | SFBmodule md ->
-      let mod_type_expr = translate_mod_type md.mod_mp env md.mod_type in
+  match e with
+  | SFBconst cb -> MSFconst l
+  | SFBmodule md -> assert false
+(*      let mod_type_expr = translate_mod_type md.mod_mp env md.mod_type in
       let uid = string_of_label l in
-      <:sig_item< module $uid:uid$ : $mod_type_expr$ >>
-  | SFBmind mb ->
-      let _,lid = mind_lid mp (mp,id_of_label l) in
-      <:sig_item< value $lid:lid$ : Nativevalues.t >>
+      <:sig_item< module $uid:uid$ : $mod_type_expr$ >>*)
+  | SFBmind mb -> assert false
+(*      let _,lid = mind_lid mp (mp,id_of_label l) in
+      <:sig_item< value $lid:lid$ : Nativevalues.t >>*)
 (*  | SFBmodtype mdtyp ->
       let tr = translate_mod_type mp env mdtyp.typ_expr in
       <:str_item< module type $uid:string_of_label l$ = $tr$ >>*)
-  | _ -> assert false*)
+  | _ -> assert false
 
 
 let rec translate_mod mp env mod_expr =
   match mod_expr with
-(*  | SEBident mp' ->
-      let uid = relative_mp_of_mp mp mp' in
+  | SEBident mp' -> assert false
+(*      let uid = relative_mp_of_mp mp mp' in
       <:module_expr< $uid$ >>, annots*)
   | SEBstruct msb ->
       let env' = add_signature mp msb empty_delta_resolver env in
@@ -65,19 +67,18 @@ let rec translate_mod mp env mod_expr =
         List.fold_left (translate_fields mp env') [] msb
       in
       MEstruct ast
-(*  | SEBfunctor (mbid,mtb,meb) ->
+  | SEBfunctor (mbid,mtb,meb) ->
       let mp' = MPbound mbid in
       let env' = add_module (module_body_of_type mp' mtb) env in
       let (_,mbid,_) = repr_mbid mbid in
-      let ast, annots = translate_mod mp env' annots meb in
+      let ast = translate_mod mp env' meb in
       let typ_ast = translate_mod_type mp' env mtb.typ_expr in
-      let e = <:module_expr< functor ($uid:mbid$ : $typ_ast$) -> $ast$ >> in
-      e, annots
-  | SEBapply (f,x,_) ->
-      let tr_f, annots = translate_mod mp env annots f in
+      MEfunctor(mbid,typ_ast,ast)
+  | SEBapply (f,x,_) -> assert false
+(*      let tr_f, annots = translate_mod mp env annots f in
       let tr_x, annots = translate_mod mp env annots x in
-      <:module_expr< $tr_f$ $tr_x$ >>, annots
-  | SEBwith _ -> assert false*)
+      <:module_expr< $tr_f$ $tr_x$ >>, annots *)
+  | SEBwith _ -> assert false
 and translate_fields mp env acc (l,x) =
   match x with
   | SFBconst cb ->
@@ -95,9 +96,7 @@ and translate_fields mp env acc (l,x) =
         | None -> assert false
       end
   | SFBmodtype mdtyp ->
-      assert false
-(*      let tr = translate_mod_type mp env mdtyp.typ_expr in
-      <:str_item< module type $uid:string_of_label l$ = $tr$ >>::ast, annots*)
+      MFmodtype(l,translate_mod_type mp env mdtyp.typ_expr)::acc
 
 let dump_library mp env mod_expr =
   print_endline "Compiling library...";
@@ -108,12 +107,35 @@ let dump_library mp env mod_expr =
       print_endline "Compiled library."; List.rev mlcode
   | _ -> assert false
 
-let pp_mod_field mp fmt t =
+
+let rec pp_mod_type_expr mp fmt mse =
+  match mse with
+  | MSEsig l -> Format.fprintf fmt "@[sig@ %a@ end@]" (pp_mod_sig_fields mp) l
+
+and pp_mod_sig_field mp fmt t =
+  match t with
+  | MSFconst l -> Format.fprintf fmt "val %s : Nativevalues.t@," (string_of_label l)
+
+and pp_mod_sig_fields mp fmt l =
+  List.iter (pp_mod_sig_field mp fmt) l
+
+let rec pp_mod_expr mp fmt me =
+  match me with
+  | MEident -> assert false
+  | MEstruct l -> Format.fprintf fmt "@[struct@ %a@ end@]" (pp_mod_fields mp) l
+  | MEfunctor(l, mse, me) ->
+      Format.fprintf fmt "functor (%s : %a) ->@ @[%a@]" l (pp_mod_type_expr mp)
+      mse (pp_mod_expr mp) me
+
+and pp_mod_field mp fmt t =
   match t with
   | MFglobal g -> pp_global mp fmt g
-  | _ -> assert false
+  | MFmodule (l,me) -> Format.fprintf fmt "module %s =@,%a@\n" (string_of_label l) (pp_mod_expr mp) me
+  | MFmodtype (l,mse) ->
+      Format.fprintf fmt "module type %s =@,%a@\n" (string_of_label l)
+      (pp_mod_type_expr mp) mse
 
-let pp_mod_fields mp fmt l =
+and pp_mod_fields mp fmt l =
   List.iter (pp_mod_field mp fmt) l
 
 let compile_module code mp load_paths f =
@@ -127,6 +149,7 @@ let compile_module code mp load_paths f =
   let fmt = Format.formatter_of_out_channel ch_out in
   pp_globals mp fmt header;
   pp_mod_fields mp fmt code;
+  Format.fprintf fmt "@.";
   let load_paths = "-I " ^ (String.concat " -I " load_paths) ^ " " in
   close_out ch_out;
   let comp_cmd =
