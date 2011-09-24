df8f4969622e94ca19a3fb603f388b8e4288ebf1
herbelin <herbelin@85f007b7-540e-0410-9357-904b9bb8a0f7>
2011-09-24 16:22:00 +0000

    Completing r14448 and thus continuing r14407 (using Cast to propagate information coming from tactics on how to solve cst/cst critical pairs in the kernel conversion machine).
    In r14448, extra Cast's were removed from kernel type-checker but
    (erroneously) not from the terms actually registered in the
    environment.
    
    The current commit completes the work by registering the term output
    by the type-checker and not the original term. Note that this needs to
    move hconsing from before to after typing.
    
    On the Coq library, propagating Cast (without keeping them on disk)
    induces a stable 1% speedup (Xeon w3540). Having hcons before or after
    type-checking makes no difference. It remains to test on user contribs
    whether the current commit compensates the slow down and vo size
    increasing coming with the improvement made to Qed in r14407.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@14492 85f007b7-540e-0410-9357-904b9bb8a0f7

--- kernel/term_typing.ml
+++ kernel/term_typing.ml
@@ -93,6 +93,9 @@ let infer_declaration env dcl =
   match dcl with
   | DefinitionEntry c ->
       let (j,cst) = infer env c.const_entry_body in
+      let j =
+        {uj_val = hcons1_constr j.uj_val;
+         uj_type = hcons1_constr j.uj_type} in
       let (typ,cst) = constrain_type env j cst c.const_entry_type in
       let def =
 	if c.const_entry_opaque
@@ -102,8 +105,8 @@ let infer_declaration env dcl =
       def, typ, cst, c.const_entry_inline_code
   | ParameterEntry (t,nl) ->
       let (j,cst) = infer env t in
-      Undef nl, NonPolymorphicType (Typeops.assumption_of_judgment env j), cst,
-      false
+      let t = hcons1_constr (Typeops.assumption_of_judgment env j) in
+      Undef nl, NonPolymorphicType t, cst, false
   | PrimitiveEntry (t,op_t) ->
       let (j,cst) = infer env t in
       check_primitive_type env op_t t; 
