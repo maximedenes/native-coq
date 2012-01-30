9a1352e225544e1ca7d5b299d24eca5392365e5d
ppedrot <ppedrot@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-01-30 18:17:13 +0000

    Added an pattern / occurence syntax for vm_compute.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@14950 85f007b7-540e-0410-9357-904b9bb8a0f7

--- proofs/redexpr.ml
+++ proofs/redexpr.ml
@@ -195,8 +195,26 @@ let rec reduction_of_red_expr = function
 	(try reduction_of_red_expr (Stringmap.find s !red_expr_tab)
 	 with Not_found ->
 	   error("unknown user-defined reduction \""^s^"\"")))
-  | CbvVm -> (cbv_vm ,VMcast)
-  | CbvNbe -> (cbv_nbe ,NATIVEcast)
+  | CbvVm (Some lp) ->
+    let b = is_reference (snd lp) in
+    let lp = out_with_occurrences lp in
+    let vmfun _ env map c =
+      let tpe = Retyping.get_type_of env map c in
+      Vnorm.cbv_vm env c tpe
+    in
+    let redfun = contextually b lp vmfun in
+    (redfun, VMcast)
+  | CbvVm None -> (cbv_vm, VMcast)
+  | CbvNbe (Some lp) ->
+    let b = is_reference (snd lp) in
+    let lp = out_with_occurrences lp in
+    let vmfun _ env map c =
+      let tpe = Retyping.get_type_of env map c in
+      Nativenorm.native_norm env c tpe
+    in
+    let redfun = contextually b lp vmfun in
+    (redfun, NATIVEcast)
+  | CbvNbe None -> (cbv_nbe, NATIVEcast)
 
 
 let subst_flags subs flags =
