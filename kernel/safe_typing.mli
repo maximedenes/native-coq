f662640c03b9f9bac296186a40fab3f9c919ae40
letouzey <letouzey@85f007b7-540e-0410-9357-904b9bb8a0f7>
2012-03-26 16:03:12 +0000

    Module names and constant/inductive names are now in two separate namespaces
     We now accept the following code: Definition E := 0. Module E. End E.
    
     Techically, we simply allow the same label to occur at most twice in
     a structure_body, which is a (label * structure_field_body) list).
     These two label occurences should not be at the same level of fields
     (e.g. a SFBmodule and a SFBmind are ok, but not two SFBmodule's or
     a SFBmodule and a SFBmodtype). Gain : a minimal amount of code change.
     Drawback : no more simple List.assoc or equivalent should be performed
     on a structure_body ...
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coq/trunk@15088 85f007b7-540e-0410-9357-904b9bb8a0f7

--- kernel/safe_typing.mli
+++ kernel/safe_typing.mli
@@ -141,7 +141,7 @@ val typing : safe_environment -> constr -> judgment
 
 (** {7 Query } *)
 
-val exists_label : label -> safe_environment -> bool
+val exists_objlabel : label -> safe_environment -> bool
 
 (* Retroknowledge of inductive *)
 
